598fa111de89e692ddec3b37e07018ec943b6acf
Tlztech <licao@aliyun.com>
2022-06-10 16:39:01 +0800

    Api V2対応

--- src/jp/co/yahoo/sample/YConnectServlet.java
+++ src/jp/co/yahoo/sample/YConnectServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import jp.co.yahoo.yconnect.YConnectExplicit;
+import jp.co.yahoo.yconnect.YConnectClient;
 import jp.co.yahoo.yconnect.core.api.ApiClientException;
 import jp.co.yahoo.yconnect.core.oauth2.AuthorizationException;
 import jp.co.yahoo.yconnect.core.oauth2.OAuth2ResponseType;
@@ -45,17 +45,20 @@ public class YConnectServlet extends HttpServlet {
 	private static final String redirectUri = "http://localhost:8000/YConnectServlet/YConnectServlet";
 	
 	private Connection con = null;
+	private String shop = null;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		response.setContentType("text/html; charset=UTF-8");
 
 		// state、nonceにランダムな値を初期化
-		String state = "5Ye65oi744KKT0vjgafjgZnjgojjgIHlhYjovKnjg4M="; // リクエストとコールバック間の検証用のランダムな文字列を指定してください
-		String nonce = "SUTljqjjga8uLi7jgrrjg4Plj4vjgaDjgoc="; // リプレイアタック対策のランダムな文字列を指定してください
-
+		String state = "5Ye65oi744KKT0vjgafjgZnjgojjgIHlhYjovKnjg4M"; // リクエストとコールバック間の検証用のランダムな文字列を指定してください
+		String nonce = "SUTljqjjga8uLi7jgrrjg4Plj4vjgaDjgoc"; // リプレイアタック対策のランダムな文字列を指定してください
+		String plainCodeChallenge =
+                "E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM._~";
+		
 		// YConnectインスタンス生成
-		YConnectExplicit yconnect = new YConnectExplicit();
+		YConnectClient yconnect = new YConnectClient();
 
 		// SSL証明書チェック無効
 		// (※Production環境では必ず有効にすること)
@@ -63,8 +66,10 @@ public class YConnectServlet extends HttpServlet {
 
 		try {
 
+			String fullUri = request.getRequestURL().toString() + "?" + request.getQueryString();
+            URI requestUri = new URI(fullUri);
 			// コールバックURLから各パラメーターを抽出
-			if (yconnect.hasAuthorizationCode(request.getQueryString())) {
+			if (yconnect.hasAuthorizationCode(requestUri)) {
 
 				/*********************************************************
 				 * Parse the Callback URI and Save the Access Token.
@@ -73,8 +78,8 @@ public class YConnectServlet extends HttpServlet {
 				StringBuffer sb = new StringBuffer();
 
 				// 認可コードを取得
-				//String code = yconnect.getAuthorizationCode(state);
-				String code = request.getParameter("code");
+				String code = yconnect.getAuthorizationCode(state);
+//				String code = request.getParameter("code");
 
 				sb.append("<h1>Authorization Request</h1>");
 				sb.append("Authorization Code: " + code + "<br/><br/>");
@@ -84,7 +89,7 @@ public class YConnectServlet extends HttpServlet {
 				 ***********************************************/
 
 				// Tokenエンドポイントにリクエスト
-				yconnect.requestToken(code, clientId, clientSecret, redirectUri);
+				yconnect.requestToken(code, clientId, clientSecret, redirectUri, plainCodeChallenge);
 				// アクセストークン、リフレッシュトークン、IDトークンを取得
 				String accessTokenString = yconnect.getAccessToken();
 				long expiration = yconnect.getAccessTokenExpiration();
@@ -102,10 +107,25 @@ public class YConnectServlet extends HttpServlet {
 
 				// IDトークンを復号化、値の検証
 				//IdTokenObject idTokenObject = yconnect.decodeIdToken(idTokenString, nonce, clientId, clientSecret);
-				IdTokenObject idTokenObject = yconnect.decodeIdToken(idTokenString);
-				
-				sb.append("<h1>ID Token</h1>");
-				sb.append("ID Token: " + idTokenObject.toString() + "<br/><br/>");
+//				IdTokenObject idTokenObject = yconnect.decodeIdToken(idTokenString);
+//				
+//				sb.append("<h1>ID Token</h1>");
+//				sb.append("ID Token: " + idTokenObject.toString() + "<br/><br/>");
+				if (yconnect.verifyIdToken(nonce, clientId, idTokenString)) {
+                    // IDトークンの復号
+                    IdTokenObject idTokenObject = yconnect.decodeIdToken(idTokenString);
+                    sb.append("<h1>ID Token</h1>");
+                    sb.append("ID Token: ").append(idTokenObject.toString()).append("<br/><br/>");
+                } else {
+                    // 検証に失敗したのでエラー文を出力
+                    sb.append("<h1>ID Token</h1>");
+                    sb.append("ID Token error: ")
+                            .append(yconnect.getIdTokenErrorMessage())
+                            .append("<br/><br/>");
+                    sb.append("ID Token error description: ")
+                            .append(yconnect.getIdTokenErrorDescriptionMessage())
+                            .append("<br/><br/>");
+                }
 
 				/*************************
 				 * Request UserInfo.
@@ -117,6 +137,22 @@ public class YConnectServlet extends HttpServlet {
 				UserInfoObject userInfoObject = yconnect.getUserInfoObject();
 				sb.append("<h1>UserInfo Request</h1>");
 				sb.append("UserInfo: <pre>" + userInfoObject + "</pre><br/>");
+				sb.append("sub: <pre>")
+                .append(userInfoObject.getAdditionalValue("sub"))
+                .append("</pre><br/>");
+				
+				// Tokenエンドポイントにリクエストしてアクセストークンを更新
+                yconnect.refreshToken(refreshToken, clientId, clientSecret);
+                accessTokenString = yconnect.getAccessToken();
+                expiration = yconnect.getAccessTokenExpiration();
+
+                sb.append("<h1>Refresh Token</h1>");
+                sb.append("Access Token: ").append(accessTokenString).append("<br/><br/>");
+                sb.append("Expiration: ").append(expiration).append("<br/><br/>");
+
+                PrintWriter out = response.getWriter();
+                out.println(new String(sb));
+                out.close();
 				
 				if (null == con || con.isClosed()) {
 					con = JdbcConnection.getConnection();
@@ -129,20 +165,16 @@ public class YConnectServlet extends HttpServlet {
 				Calendar date = Calendar.getInstance();
 				String updateTime = sdf.format(date.getTime());
 				ps.setString(3, updateTime);
-				ps.setString(4, clientSecret);
+				ps.setString(4, shop);
 				ps.execute();
 				con.close();
-
-				PrintWriter out = response.getWriter();
-				out.println(new String(sb));
-				out.close();
 
 			} else {
 				
 				if (null == con || con.isClosed()) {
 					con = JdbcConnection.getConnection();
 				}
-				String shop = request.getParameter("shop");
+				shop = request.getParameter("shop");
 				String sql = "SELECT YAHOO_APP_ID FROM rakuten.shop WHERE SITE = 'Yahoo' AND SHOP_ID = ?";
 				PreparedStatement ps = con.prepareStatement(sql);
 				ps.setString(1, shop);
@@ -152,21 +184,22 @@ public class YConnectServlet extends HttpServlet {
 					clientId = rs.getString("YAHOO_APP_ID");
 				}
 				
-				clientSecret = shop;
+				clientSecret = "P4WSqo0lSZ6klF4FbRqLzNvzxh3Hwgvp3C0J1CHg";
 
 				/****************************************************************
 				 * Request Authorization Endpoint for getting Access Token.
 				 ****************************************************************/
 
 				// 各パラメーター初期化
-				String responseType = OAuth2ResponseType.CODE_IDTOKEN;
-				String display = OIDCDisplay.DEFAULT;
-				String[] prompt = { OIDCPrompt.DEFAULT };
-				String[] scope = { OIDCScope.OPENID, OIDCScope.PROFILE,
-						OIDCScope.EMAIL, OIDCScope.ADDRESS };
+				String responseType = OAuth2ResponseType.CODE;
+                String display = OIDCDisplay.DEFAULT;
+                String[] prompt = {OIDCPrompt.DEFAULT};
+                String[] scope = {
+                    OIDCScope.OPENID, OIDCScope.PROFILE, OIDCScope.EMAIL, OIDCScope.ADDRESS
+                };
 
 				// 各パラメーターを設定
-				yconnect.init(clientId, redirectUri, state, responseType, display, prompt, scope, nonce);
+				yconnect.init(clientId, redirectUri, state, responseType, display, prompt, scope, nonce, 3600L, plainCodeChallenge);
 				URI uri = yconnect.generateAuthorizationUri();
 
 				// Authorizationエンドポイントにリダイレクト(同意画面を表示)
